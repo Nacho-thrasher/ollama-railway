bb36544359fa5ceec724e411402125fc1dcea030
Jorge Ignacio Burgos <jiburgos@ucasal.local>
2025-05-21 11:15:03 -0300

    fix serve 2

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@
 #socat TCP-LISTEN:8080,fork TCP:localhost:11434 &
 
 # Iniciar Ollama con menor uso de memoria (usa mmap si es posible)
-exec ollama serve \
-  --ctx-size 2048 \
-  --batch-size 256
+exec ollama serve 
+# --ctx-size 2048 
+# --batch-size 256
 
 # Start Ollama
 # exec serve
