064bf7200f871ec01b8f278caead98deabad6766
Jorge Ignacio Burgos <jiburgos@ucasal.local>
2025-05-21 14:12:47 -0300

    fix models carga

--- start.sh
+++ start.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-# Start socat in the background to forward port 8080 to 11434
-#socat TCP-LISTEN:8080,fork TCP:localhost:11434 &
-
-# Iniciar Ollama con menor uso de memoria (usa mmap si es posible)
-exec ollama serve 
-# --ctx-size 2048 
-# --batch-size 256
-
-# Start Ollama
-# exec serve
+# Iniciar Ollama con menor uso de RAM, mmap deshabilitado, y tamaño de contexto ajustado
+exec ollama serve \
+  --model phi3:latest \
+  --ctx-size 2048 \
+  --batch-size 256 \
+  --threads 8 \
+  --no-mmap \
+  --numa \
+  --parallel
